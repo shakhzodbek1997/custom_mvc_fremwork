4938455b166a5fbe77be9e8b22be8ac133d9c8fe
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-24 11:45:30 +0200

    Loading The Controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -8,14 +8,31 @@
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
-
     public function __construct(){
-      $this->getUrl(); 
-    }
+      $url = $this->getUrl();
 
-    public function getUrl(){
-      echo $_GET['url'];
+      // Look in controllers for first value
+      if(file_exists('../app/controllers/' . ucwords($url[0]. 'php'))){
+        // iF exists, set as controller
+        $this->currentController = ucwords($url[0]);
+
+        //Unset 0 index
+        unset($url[0]);
+      }
+
+      // Require the Controller
+      require_once '../app/controllers/'. $this->currentController . '.php';
+
+      // Instantiate controller class
+      $this->currentController = new $this->currentController;
     }
-  } 
-  
-  
+    public function getUrl(){
+      if(isset($_GET['url'])){
+        $url = rtrim($_GET['url']. '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+      }
+    }
+  }
+
