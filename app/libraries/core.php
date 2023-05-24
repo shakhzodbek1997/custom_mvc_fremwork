f129292bbd47ff0658ca5c6f5bedc663ba46bfd2
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-24 12:03:35 +0200

    mapping methods and parameters

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -24,7 +24,24 @@
       require_once '../app/controllers/'. $this->currentController . '.php';
 
       // Instantiate controller class
-      $this->currentController = new $this->currentController;
+      $this->currentController = new $this->currentController;       
+
+      // Check for second part of URL
+      if(isset($url[1])){
+        // Check to see if method exists in controller
+        if(method_exists($this->currentController, $url[1])){
+          $this->currentMethod = $url[1];
+
+          // Unset 1 index
+          unset($url[1]);
+        }
+      }
+
+      //  Get params
+      $this->params = $url ? array_values($url) : [];
+
+      // Call a callback with array of params
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params); 
     }
     public function getUrl(){
       if(isset($_GET['url'])){
