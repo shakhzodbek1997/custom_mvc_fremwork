36ddf8e7e457d51543e808357503607ec0373cb5
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-25 13:52:47 +0200

    clea n ed up the project

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_NAME', 'tmvc');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_NAME', '__YOUR_DB_NAME__');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/traversymvc');
+  define('URLROOT', '__YOUR_URL__');
   // Site Name
-  define('SITENAME', 'TraversyMVC');
+  define('SITENAME', '__YOUR_SITENAME__');
