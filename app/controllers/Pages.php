111216b5967f3487ec397808ed5ff84f3d7ef02b
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-25 17:10:11 +0200

    cleaned up

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,7 +6,7 @@
     
     public function index(){
        $data = [
-        'title' => 'TraversyMVC    ',
+        'title' => 'TraversyMVC',
       ];
      
       $this->view('pages/index', $data);
