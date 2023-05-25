b97adcd7079392f4088a099e48beecef34c9a2e1
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-25 02:27:06 +0200

    loading views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,18 @@
-<?php 
- class Pages extends Controller {
-  public function __construct(){
+<?php
+  class Pages extends Controller {
+    public function __construct(){
 
-  }
+    }
 
-  public function index(){
-    
+    public function index(){
+      $data = [
+        'title' => 'Welcome'
+      ];
+
+      $this->view('pages/index', $data);
+    }
+
+    public function about(){
+      $this->view('pages/about');
+    }
   }
-  
-  public function about($id){
-     echo $id; 
-  }
- }
